400031f9d00135fa26a2f5f6abcd782e18217e24
s13005 <s13005@std.it-college.ac.jp>
2014-11-05 15:24:35 +0900

    11/05

--- 141105-1.sql
+++ 141105-1.sql
@@ -1,18 +1,18 @@
 create table emp_copy as select * from employees
 /
 
-update employees
+update emp_copy
 set deptno = 10
 where deptno is null
 /
 
 select deptno
-from employees
+from emp_copy
 /
 
 rollback
 /
 
 select deptno
-from employees
+from emp_copy
 /
