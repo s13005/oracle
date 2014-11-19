edb26a7b054fa2618771406c73197aaf2eb420c7
s13005 <s13005@std.it-college.ac.jp>
2014-11-19 15:51:34 +0900

    11/19

--- Select1.java
+++ Select1.java
@@ -4,13 +4,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
+import java.util.Scanner;
 
 public class Select1 {
+	Scanner stdIn = new Scanner(System.in);
 	private String _user = "s13005";
 	private String _pass = "password";
 	private String _host = "172.16.40.4";
 	private String _sid = "db11";
-	
+
 	public static void main(String[] args){
 
 		Select1 select1 = new Select1();
@@ -32,9 +34,10 @@ public class Select1 {
 			conn = DriverManager.getConnection(
 					"jdbc:oracle:thin:@" + _host + ":1521:" + _sid, _user, _pass);
 
-			ps = conn.prepareStatement("select e.empno,e.ename, e.job, m.ename,dname, loc from employees e left outer join employees m ON e.mgr = m.empno join departments d on e.deptno = d.deptno");
+			ps = conn.prepareStatement("SELECT e.empno, e.ename, e.job, m.ename, d.dname, d.loc, e.sal, s.grade FROM employees e LEFT JOIN employees m ON e.mgr = m.empno JOIN departments d ON e.deptno = d.deptno JOIN salgrades s ON e.sal BETWEEN s.losal AND s.hisal");
 
 			rs = ps.executeQuery();
+
 			while(rs.next()){
 				String empno = rs.getString(1);
 				String ename = rs.getString(2);
@@ -42,8 +45,31 @@ public class Select1 {
 				String ename2 = rs.getString(4);
 				String dname = rs.getString(5);
 				String loc = rs.getString(6);
+				String e_sal = rs.getString(7);
+				String s_grade = rs.getString(8);
 
-				System.out.printf("社員番号:%s\t社員名:%s\t職種:%s\t上司:%s\t部署名:%s\t場所:%s\n", empno, ename, job, ename2, dname, loc);
+				System.out.printf("社員番号:%s\t社員名:%s\t職種: %s\t上司:%s\t部署名:%s\t場所:%s\t給与:%s\t給与の等級:%s\n", empno,ename, job, ename2, dname, loc, e_sal, s_grade);
+			}
+
+			System.out.print("empno:");
+			ps = conn.prepareStatement("SELECT e.empno, e.ename, e.job, m.ename, d.dname, d.loc, e.sal, s.grade FROM employees e LEFT JOIN employees m ON e.mgr = m.empno JOIN departments d ON e.deptno = d.deptno JOIN salgrades s ON e.sal BETWEEN s.losal AND s.hisal WHERE e.empno = ?");
+
+			int empno = stdIn.nextInt();
+			ps.setInt(1, empno);
+			rs = ps.executeQuery();
+
+			while(rs.next()){
+				String e_empno = rs.getString(1);
+				String e_ename = rs.getString(2);
+				String e_job = rs.getString(3);
+				String m_ename = rs.getString(4);
+				String d_dname = rs.getString(5);
+				String d_loc = rs.getString(6);
+				String e_sal = rs.getString(7);
+				String s_grade = rs.getString(8);
+
+
+				System.out.printf("社員番号： %s\t社員名： %s\t職種:  %s\t上司: %s\t部署名: %s\t場所 %s\t給与 %s\t給与の等級  %s\n", e_empno, e_ename, e_job, m_ename, d_dname, d_loc, e_sal, s_grade);
 			}
 		}catch(ClassNotFoundException e){
 			throw e;
