7039f6ef93187459f26596176493377d236da61e
s13005 <s13005@std.it-college.ac.jp>
2014-11-19 15:54:51 +0900

    11/19

--- a.sql
+++ a.sql
@@ -1,6 +1,7 @@
-select e.empno,e.ename, e.job, m.ename,dname, loc
+select e.empno,e.ename, e.job,e.sal,e.comm, m.ename,dname, loc
 from employees e left outer join employees m 
 ON e.mgr = m.empno
 join departments d
 on e.deptno = d.deptno
+where e.empno = &empno
 /
